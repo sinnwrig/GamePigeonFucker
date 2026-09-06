d956be7796ecd43f5d411e0ce7cf3539a82dfb66
Kai Angulo <kangriebling@gmail.com>
2026-09-06 12:57:59 -0700

    Delete old run.sh file

--- MessagesInjector/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DIR="$(cd "$(dirname "$0")" && pwd)"
-killall Messages 2>/dev/null
-sleep 1
-DYLD_INSERT_LIBRARIES="$DIR/libMessagesInjector.dylib" /System/Applications/Messages.app/Contents/MacOS/Messages &
