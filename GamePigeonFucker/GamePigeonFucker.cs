bebd50db155c29f996821b55d875f5cb5aa5c672
Kai Angulo <kangriebling@gmail.com>
2026-09-07 13:57:10 -0700

    Cut out logic from entrypoint class

--- GamePigeonFucker/GamePigeonFucker.cs
+++ GamePigeonFucker/GamePigeonFucker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Text;
 using IMessage;
@@ -6,13 +6,13 @@ using IMessage;
 
 public static class GamePigeonFucker
 {
-    static MessagingService? Service;
-    static string? ActiveAccount;
-    static string? ActiveAlias;
+    public static MessagingService? Service { get; private set; }
+    public static Options Options { get; private set; }
+    public static string? IncomingChatIdentifier { get; private set; }
+
     static CancellationTokenSource? WatchCts;
     static SshInjectorTunnel? ActiveTunnel;
     static CommandConsole CConsole;
-    static Options Options;
     static Gamer Gamer;
 
 
@@ -20,17 +20,7 @@ public static class GamePigeonFucker
     {
         CConsole = new("GPF v0.1.0 - Kai Angulo");
         Options = new("options.json");
-
-        var playerUuid = Options.GetString("GamePigeonPlayerUuid");
-        if (string.IsNullOrEmpty(playerUuid))
-        {
-            playerUuid = Guid.NewGuid().ToString();
-            Options.Set("GamePigeonPlayerUuid", playerUuid);
-        }
-
-        var playerAvatar = Options.GetString("GamePigeonAvatar", "");
-
-        Gamer = new Gamer(playerUuid, playerAvatar!);
+        Gamer = new Gamer();
 
         BannerPrinter.Print("GAME PIGEON FUCKER V1");
 
@@ -38,8 +28,7 @@ public static class GamePigeonFucker
 
         Console.WriteLine("Initialized IMessage service.");
 
-        ActiveAccount = Options.GetString("ActiveAccount", "");
-        ActiveAlias = Options.GetString("ActiveAlias", "");
+        Account.LoadActiveFromOptions(Options);
 
         CConsole.BeginConsole();
     }
@@ -100,8 +89,7 @@ public static class GamePigeonFucker
 
         Service = new MessagingService(transport);
         Service.OnReceiveMessage += LogMessage;
-        ActiveAccount = "";
-        ActiveAlias = "";
+        Account.SetActive(null, null);
         StartWatching();
 
         Options.Set("Remote", host);
@@ -119,141 +107,23 @@ public static class GamePigeonFucker
         if (msg.Text.StartsWith('/'))
         {
             CConsole.OnMessage = BuildText;
+            IncomingChatIdentifier = msg.ChatIdentifier;
 
             CConsole.TryExecute(msg.Text, out string? error);
             Console.WriteLine(error);
 
+            IncomingChatIdentifier = null;
             CConsole?.OnMessage = default;
         }
 
-        if (sb.Length != 0)
-            SendMessage(msg.ChatIdentifier, sb.ToString());
+        if (sb.Length > 0 && sb[^1] == '\\')
+            sb.Length--;
+
+        string text = sb.ToString().TrimEnd();
+
+        if (text.Length != 0)
+            Account.SendMessage(msg.ChatIdentifier, text);
 
         Gamer.HandleMessage(msg, Service!);
-    }
-
-
-    public enum Operation
-    {
-        set,
-        find,
-        list,
-        active,
-    }
-
-
-    [Command("/account", "Account utilities")]
-    public static async void Account(
-    [CommandParam] Operation type,
-    [CommandParam, IntCondition(nameof(type), (long)Operation.find)] string findArg,
-    [CommandParam, IntCondition(nameof(type), (long)Operation.set)] string setArg,
-    [CommandParam(optional: true), IntCondition(nameof(type), (long)Operation.set)] string alias
-    )
-    {
-        if (type == Operation.active)
-        {
-            if (ActiveAccount != null)
-                Console.WriteLine($"Active account alias: {ActiveAlias}");
-            else
-                Console.WriteLine("No active account set");
-
-            return;
-        }
-
-        var accounts = await Service!.ListAccountsAsync();
-
-        void LogAccount(ImAccountInfo acc)
-        {
-            Console.WriteLine($"Account: {acc.AliasList.ElementAtOrDefault(0)}");
-            Console.WriteLine($"\tAliases: {string.Join(',', acc.AliasList)}");
-            Console.WriteLine($"\tClass name: {acc.ClassName}");
-            Console.WriteLine($"\tLogin Handle: {acc.LoginImHandle}");
-            Console.WriteLine($"\tLogin Handles: {string.Join(',', acc.LoginHandleList)}");
-            Console.WriteLine($"\tUnique ID: {acc.UniqueId}");
-        }
-
-        switch (type)
-        {
-            case Operation.find:
-                var account = accounts.FirstOrDefault(x => x?.AliasList.Contains(findArg) ?? false, null);
-
-                if (account != null)
-                    LogAccount(account);
-                else
-                    Console.WriteLine($"Could not find account with alias matching: {findArg}");
-
-                break;
-
-            case Operation.list:
-                Console.WriteLine($"{accounts.Count} accounts found");
-
-                foreach (var acc in accounts)
-                    LogAccount(acc);
-
-                break;
-
-            case Operation.set:
-                var setAccount = accounts.FirstOrDefault(x => x?.Aliases.Contains(setArg) ?? false, null);
-
-                if (setAccount != null)
-                {
-                    Console.WriteLine($"Setting account: {setAccount.AliasList.ElementAtOrDefault(0)}");
-                    ActiveAccount = setAccount.UniqueId;
-                    ActiveAlias = setAccount.AliasList.FirstOrDefault();
-
-                    if (!string.IsNullOrEmpty(alias))
-                    {
-                        for (int i = 0; i < setAccount.AliasList.Count; i++)
-                            if (setAccount.AliasList[i] == alias)
-                            {
-                                ActiveAlias = setAccount.AliasList[i];
-                                break;
-                            }
-                    }
-
-                    Options.Set("ActiveAccount", setAccount.UniqueId);
-                    Options.Set("ActiveAlias", ActiveAlias);
-                }
-                else
-                {
-                    Console.WriteLine($"Could not find account with alias: {setArg}");
-                }
-
-                break;
-        }
-    }
-
-
-    [Command("/message", "It's about sending a message, batman")]
-    public static async void SendMessage([CommandParam] string recipient, [CommandParam] string message)
-    {
-        if (ActiveAccount == null)
-        {
-            Console.WriteLine("No active account set");
-            return;
-        }
-
-        await Service!.SendMessageAsync(recipient,
-            new OutboundMessage(message, SenderAccountUniqueId: ActiveAccount, SenderIdentityId: ActiveAlias));
-    }
-
-
-    public enum OnOff
-    {
-        on,
-        off
-    }
-
-
-    [Command("/hax", "turns on hax")]
-    public static void Hax([CommandParam] OnOff onOff, [CommandParam] string hack)
-    {
-        if (!Gamer.SetEnabled(hack, onOff == OnOff.on))
-        {
-            Console.WriteLine($"Unknown solver '{hack}'. Available: {string.Join(", ", Gamer.SolverNames)}");
-            return;
-        }
-
-        Console.WriteLine($"{hack} hax turned {onOff}");
     }
 }
