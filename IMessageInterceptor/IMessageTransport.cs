6deab746fb5ccc52f54a134b1344a2b5c4fcc0d7
Kai Angulo <kangriebling@gmail.com>
2026-09-16 10:25:57 -0700

    Add event watcher for chat events

--- IMessageInterceptor/IMessageTransport.cs
+++ IMessageInterceptor/IMessageTransport.cs
@@ -4,6 +4,9 @@ public interface IMessageTransport
 {
     Task WatchAsync(Action<InboundMessage> onMessage, CancellationToken cancellationToken);
 
+    /// <summary>Full classified event stream (messages, reactions, receipts, delivery status, edits, resyncs).</summary>
+    Task WatchEventsAsync(Action<ChatEvent> onEvent, CancellationToken cancellationToken);
+
     Task SendAsync(string chatIdentifier, OutboundMessage message);
 
     Task<IReadOnlyList<ImAccountInfo>> ListAccountsAsync();
